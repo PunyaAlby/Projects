dc49098fc14ca83e5267e69ac222dd03ba634b50
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-07-09 09:14:49 +0700

    Update ping.py

--- AyiinXd/modules/ping.py
+++ AyiinXd/modules/ping.py
@@ -50,21 +50,21 @@ async def get_readable_time(seconds: int) -> str:
 async def _(ping):
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    Ayiin = await edit_or_reply(ping, "**✧**")
-    await Ayiin.edit("**✧✧**")
-    await Ayiin.edit("**✧✧✧**")
-    await Ayiin.edit("**✧✧✧✧**")
-    await Ayiin.edit("**✧✧✧✧✧**")
+    Ayiin = await edit_or_reply(ping, "**㊪**")
+    await Ayiin.edit("**㊪㊪**")
+    await Ayiin.edit("**㊪㊪㊪**")
+    await Ayiin.edit("**㊪㊪㊪㊪**")
+    await Ayiin.edit("**㊪㊪㊪㊪㊪**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     user = await ping.client.get_me()
     await Ayiin.edit("⚡")
     sleep(3)
     await Ayiin.edit(
-        f"**✧ 𝙰𝚈𝙸𝙸𝙽-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ✧**\n\n"
-        f"✧ **𝙿𝙸𝙽𝙶𝙴𝚁 :** `%sms`\n"
-        f"✧ **𝚄𝙿𝚃𝙸𝙼𝙴 :** `{uptime}` \n"
-        f"✧ **𝙾𝚆𝙽𝙴𝚁 :** [{user.first_name}](tg://user?id={user.id})" % (duration)
+        f"**㊪ ᴀʟʙʏ-ᴜsᴇʀʙᴏᴛ ㊪**\n\n"
+        f"㊪ **𝙿𝙸𝙽𝙶𝙴𝚁 :** `%sms`\n"
+        f"㊪ **𝚄𝙿𝚃𝙸𝙼𝙴 :** `{uptime}` \n"
+        f"㊪ **𝙾𝚆𝙽𝙴𝚁 :** [{user.first_name}](tg://user?id={user.id})" % (duration)
     )
 
 
