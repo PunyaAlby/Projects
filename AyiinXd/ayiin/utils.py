aff7fc4ff0f8271a5d3054022658ded7e55e96df
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2023-07-23 04:10:06 +0700

    Update utils.py

--- AyiinXd/ayiin/utils.py
+++ AyiinXd/ayiin/utils.py
@@ -1,3 +1,171 @@
+# Credits: @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+#
+# autopilot by @kenkan
+
+import asyncio
+import importlib
+import logging
+import random
+import sys
+from pathlib import Path
+from random import randint
+
+
+import heroku3
+from telethon.tl.functions.contacts import UnblockRequest
+from telethon.errors import ChannelsTooMuchError
+from telethon.tl.functions.channels import CreateChannelRequest, EditPhotoRequest
+from telethon.tl.types import ChatPhotoEmpty, InputChatUploadedPhoto
+from telethon.utils import get_peer_id
+
+from AyiinXd import (
+    BOT_TOKEN,
+    BOTLOG_CHATID,
+    CMD_HELP,
+    HEROKU_API_KEY,
+    HEROKU_APP_NAME,
+    LOGS,
+    bot,
+)
+
+from .tools import download_file
+
+
+heroku_api = "https://api.heroku.com"
+if HEROKU_APP_NAME is not None and HEROKU_API_KEY is not None:
+    Heroku = heroku3.from_key(HEROKU_API_KEY)
+    app = Heroku.app(HEROKU_APP_NAME)
+    heroku_var = app.config()
+else:
+    app = None
+
+
+async def autopilot():
+    LOGS.info("TUNGGU SEBENTAR. SEDANG MEMBUAT GROUP LOG USERBOT UNTUK ANDA")
+    if BOTLOG_CHATID and str(BOTLOG_CHATID).startswith("-100"):
+        return
+    y = []  # To Refresh private ids
+    async for x in bot.iter_dialogs():
+        y.append(x.id)
+    if BOTLOG_CHATID:
+        try:
+            await bot.get_entity(int("BOTLOG_CHATID"))
+            return
+        except BaseException:
+            del heroku_var["BOTLOG_CHATID"]
+    try:
+        r = await bot(
+            CreateChannelRequest(
+                title="ᴀʟʙʏ ᴜꜱᴇʀʙᴏᴛ ʟᴏɢs",
+                about="» Group log Created by: ALBY-Userbot\n\n» Support : @ruangdiskusikami\n» UPDATES: @ruangprojects",
+                megagroup=True,
+            ),
+        )
+    except ChannelsTooMuchError:
+        LOGS.info(
+            "Terlalu banyak channel dan grup, hapus salah satu dan restart lagi"
+        )
+        exit(1)
+    except BaseException:
+        LOGS.info(
+            "Terjadi kesalahan, Buat sebuah grup lalu isi id nya di config var BOTLOG_CHATID."
+        )
+        exit(1)
+    chat = r.chats[0]
+    channel = get_peer_id(chat)
+    if isinstance(chat.photo, ChatPhotoEmpty):
+        photo = await download_file(
+            "https://telegra.ph/file/cbe826936d4de9ec1838a.jpg", "photoyins.jpg"
+        )
+        ll = await bot.upload_file(photo)
+        try:
+            await bot(
+                EditPhotoRequest(int(channel), InputChatUploadedPhoto(ll))
+            )
+        except BaseException as er:
+            LOGS.exception(er)
+    if not str(chat.id).startswith("-100"):
+        heroku_var["BOTLOG_CHATID"] = "-100" + str(chat.id)
+    else:
+        heroku_var["BOTLOG_CHATID"] = str(chat.id)
+
+
+async def autobot():
+    if BOT_TOKEN:
+        return
+    await bot.start()
+    await asyncio.sleep(15)
+    await bot.send_message(
+        BOTLOG_CHATID, "**TUNGGU SEBENTAR SEDANG MEMBUAT ASSISTANT BOT ANDA DI @BotFather**"
+    )
+    LOGS.info("TUNGGU SEBENTAR. SEDANG MEMBUAT ASSISTANT BOT UNTUK ANDA")
+    who = await bot.get_me()
+    name = f"{who.first_name} Assistant Bot"
+    if who.username:
+        username = f"{who.username}_bot"
+    else:
+        username = f"alby{(str(who.id))[5:]}bot"
+    bf = "@BotFather"
+    await bot(UnblockRequest(bf))
+    await bot.send_message(bf, "/cancel")
+    await asyncio.sleep(1)
+    await bot.send_message(bf, "/start")
+    await asyncio.sleep(1)
+    await bot.send_message(bf, "/newbot")
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    if isdone.startswith("That I cannot do."):
+        LOGS.info(
+            "Silakan buat Bot dari @BotFather dan tambahkan tokennya di var BOT_TOKEN"
+        )
+        sys.exit(1)
+    await bot.send_message(bf, name)
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    if not isdone.startswith("Good."):
+        await bot.send_message(bf, "My Assistant Bot")
+        await asyncio.sleep(1)
+        isdone = (await bot.get_messages(bf, limit=1))[0].text
+        if not isdone.startswith("Good."):
+            LOGS.info(
+                "Silakan buat Bot dari @BotFather dan tambahkan tokennya di var BOT_TOKEN"
+            )
+            sys.exit(1)
+    filogo = random.choice(
+        [
+            "https://telegra.ph/file/cbe826936d4de9ec1838a.jpg",
+            "AyiinXd/resources/ALBY.jpg",
+        ]
+    )
+    await bot.send_message(bf, username)
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    await bot.send_read_acknowledge("botfather")
+    if isdone.startswith("Sorry,"):
+        ran = randint(1, 100)
+        username = f"alby{(str(who.id))[6:]}{str(ran)}bot"
+        await bot.send_message(bf, username)
+        await asyncio.sleep(1)
+        nowdone = (await bot.get_messages(bf, limit=1))[0].text
+        if nowdone.startswith("Done!"):
+            token = nowdone.split("`")[1]
+            await bot.send_message(bf, "/setinline")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, "Search")
+            await asyncio.sleep(3)
+            await bot.send_message(bf, "/setuserpic")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_file(bf, "AyiinXd/resources/ALBY.jpg")
+            await asyncio.sleep(3)
+            await bot.send_message(bf, "/setabouttext")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
             await asyncio.sleep(1)
             await bot.send_message(bf, f"Managed With ✨ By {who.first_name}")
             await asyncio.sleep(3)
